8c7c0efa5010d312945990ac85ddc2941abe805f
ArthurD <artur2134@mail.ru>
2023-12-09 14:53:16 +0300

    Переделал структуру проекта

--- src/Cnavas.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-import java.awt.*;
-
-public class Cnavas extends JPanel {
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-    }
-}
--- src/Tetris.java
+++ src/Tetris.java
@@ -19,7 +19,7 @@ public class Tetris {
     final int UP = 38;
     final int RIGHT = 39;
     final int DOWN = 40;
-    final int SHOW_DELAY = 350; // определяет задержку анимации
+    final int SHOW_DELAY = 400; // определяет задержку анимации
 
     final int[][][] SHAPES = {
             {{0, 0, 0, 0}, {1, 1, 1, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}, {4, 0x00f0f0}}, // I
@@ -28,7 +28,7 @@ public class Tetris {
             {{0, 0, 1, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {3, 0xf0a000}}, // L
             {{0, 1, 1, 0}, {1, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {3, 0x00f000}}, // S
             {{1, 1, 1, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {3, 0xa000f0}}, // T
-            {{1, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {3, 0xf00000}} // Z
+            {{1, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {3, 0xf00000}}  // Z
     };
     final int[] SCORES = {100, 300, 700, 1500}; // количество очков за заполнение строк в игре.
     int gameScores = 0; // для хранение очков
@@ -60,7 +60,7 @@ public class Tetris {
         new Tetris().go();
     }
 
-    void go() {
+    public Tetris() {
         frame = new JFrame(TITLE_OF_PROGRAM);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(FIELD_WIDTH * BLOCK_SIZE + FIELD_DX, FIELD_HEIGHT * BLOCK_SIZE + FIELD_DY);
@@ -85,7 +85,9 @@ public class Tetris {
         frame.setVisible(true);
 
         Arrays.fill(mine[FIELD_HEIGHT], 1);
+    }
 
+    void go() {
         while (!gameOver) {
             try {
                 Thread.sleep(SHOW_DELAY);
@@ -105,5 +107,100 @@ public class Tetris {
     // проверка на заполнение строк
     void checkFilling() {
 
+    }
+
+    class Canvas extends JPanel {
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+            figure.paint(g);
+
+        }
+    }
+
+    class Figure {
+        private ArrayList<Block> figure = new ArrayList<>();
+        private int[][] shape = new int[4][4];
+        private int type, size, color; // тип, размер. цвет фигуры соответственно
+        private int x = 3, y = 0; // координаты фигуры
+
+        public Figure() {
+            type = random.nextInt(SHAPES.length);
+            size = SHAPES[type][4][0];
+            color = SHAPES[type][4][1];
+            if (size == 4) y = -1;
+            for (int i = 0; i < size; i++)
+                System.arraycopy(SHAPES[type][i], 0, shape[i], 0, SHAPES[type][i].length);
+            createFromShape();
+        }
+
+        private void createFromShape() {
+            for (int x = 0; x < size; x++) {
+                for (int y = 0; y < size; y++) {
+                    if (shape[y][x] == 1) figure.add(new Block(x + this.x, y + this.y));
+                }
+            }
+        }
+
+        public void rotate() {
+        }
+
+        public void drop() {
+        }
+
+        public void move(int direction) {
+        }
+
+        // проверяет, касается ли фигура нижней части игравого поля или другой фигуры
+        public boolean isTouchGround() {
+            return false;
+        }
+
+        // оставляет фигуру на месте, куда она упала
+        public void leaveOnTheGround() {
+        }
+
+        // прверяет, на переполнение нашего игравого поля
+        public boolean isCrossGround() {
+            return false;
+        }
+
+        // опускает фигуру на один уровень
+        public void stepDown() {
+        }
+
+        public void paint(Graphics g) {
+            for (Block block : figure) block.paint(g, color);
+        }
+    }
+
+    class Block {
+        private int x, y;
+
+        public Block(int x, int y) {
+            setX(x);
+            setY(y);
+        }
+
+        public void paint(Graphics g, int color) {
+            g.setColor(new Color(color));
+            g.drawRoundRect(x * BLOCK_SIZE + 1, y * BLOCK_SIZE + 1, BLOCK_SIZE - 2, BLOCK_SIZE - 2, ARC_RADIUS, ARC_RADIUS);
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
     }
 }
